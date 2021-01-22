c13106c006be5d0cb1c56423464dea701ca43fbc
mily datta <dmily270@gmail.com>
2021-01-22 19:58:07 +0600

    first problem solve

---
+++ assignment.js
@@ -0,0 +1,16 @@
+// problem-1 (solve)
+//kilometerToMeter
+
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+}
+
+var kilometer = kilometerToMeter(5);
+console.log(kilometer);
+
+
+// problem-2 (solve)
+// budgetCalculator
+
+
